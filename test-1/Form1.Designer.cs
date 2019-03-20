82d5821f36545b0188eec07073e35d46730e5df7
bemoxpro <39486524+bemoxpro@users.noreply.github.com>
2019-03-20 23:17:41 +0300

    Add files via upload

---
+++ test-1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace test_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.save = new System.Windows.Forms.SaveFileDialog();
+            this.saveFile = new System.Windows.Forms.Button();
+            this.text = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // save
+            // 
+            this.save.Filter = "Text Files | *.txt";
+            // 
+            // saveFile
+            // 
+            this.saveFile.Location = new System.Drawing.Point(48, 370);
+            this.saveFile.Name = "saveFile";
+            this.saveFile.Size = new System.Drawing.Size(134, 23);
+            this.saveFile.TabIndex = 0;
+            this.saveFile.Text = "Save file";
+            this.saveFile.UseVisualStyleBackColor = true;
+            this.saveFile.Click += new System.EventHandler(this.SaveFile_Click);
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(12, 12);
+            this.text.Multiline = true;
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(614, 352);
+            this.text.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            this.openFile.Filter = "All files | *.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.saveFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Small editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog save;
+        private System.Windows.Forms.Button saveFile;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFile;
+    }
+}
+
---
+++ test-1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace test_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveFile_Click(object sender, EventArgs e)
+        {
+                
+                if(save.ShowDialog() == DialogResult.OK)
+                {
+                    save.Filter = "Text Files | *.txt";
+                    save.DefaultExt = "txt";
+                    File.WriteAllText(save.FileName, text.Text);
+                }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+                if(openFile.ShowDialog() == DialogResult.OK)
+                text.Text = File.ReadAllText(openFile.FileName);
+
+        }
+    }
+}
